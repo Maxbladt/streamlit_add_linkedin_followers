284cbb0f714444d0afed53cdee2946b6006090ff
Maxbladt <99587808+Maxbladt@users.noreply.github.com>
2023-06-25 23:38:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,12 @@ with st.echo():
     from selenium.webdriver.chrome.options import Options
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
+    from selenium.webdriver.support.ui import WebDriverWait
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.common.by import By
+
+
+
 
     @st.experimental_singleton
     def get_driver():
